26daac1bd95f04630a4fb4d7ce1d747db2496251
thadavale <thadavale@ufl.edu>
2023-10-25 20:43:31 -0400

    hello

--- lab_6_.py
+++ lab_6_.py
@@ -3,6 +3,14 @@
 # Lab 6 Encode Decode
 
 
+encoded_numbers = []
+
 def encode(decoded_numbers):
-    for num in decoded_numbers:
-        num += 3
+    global encoded_numbers
+    for num in range(0, len(decoded_numbers)):
+        new_num = int(num) + 4
+        encoded_numbers.append(new_num)
+    return encoded_numbers
+
+user_input = input()
+print(encode(user_input))
