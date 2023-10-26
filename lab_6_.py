67a8cb23e58d170abf852f8f700da731a468b04b
thadavale <thadavale@ufl.edu>
2023-10-25 20:58:41 -0400

    hello

--- lab_6_.py
+++ lab_6_.py
@@ -8,6 +8,12 @@ def encode(decoded_numbers):
     global encoded_numbers
     for num in decoded_numbers:
         new_num = int(num) + 3                          # Adds 3 to every number in the decoded numbers list
+        if new_num in range (7, 136780):
+            new_num = new_num % 10
         encoded_numbers.append(str(new_num))
-    return encoded_numbers
+    encoded_numbers = ''.join(encoded_numbers)
+    return str(encoded_numbers)
 
+input = input()
+
+print(encode(input))
