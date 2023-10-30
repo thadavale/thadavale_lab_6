f600c334b373596db7dee32e05e76796aaebd423
cherrypicked12 <zhouzhoujiang12@gmail.com>
2023-10-30 17:49:17 -0400

    hi

--- lab_6_.py
+++ lab_6_.py
@@ -17,3 +17,17 @@ def encode(decoded_numbers):
 input = input()
 
 print(encode(input))
+
+# Alice Jiang
+
+def decode(encoded_pass):
+    decoded_pass = ""
+    for num in range(0, len(encoded_pass)):
+        if int(encoded_pass[num]) - 3 < 0:
+            decoded_num = str(10 + (int(encoded_pass[num])) - 3)
+        else:
+            decoded_num = str(int(encoded_pass[num]) - 3)
+        decoded_pass += decoded_num
+    return decoded_pass
+
+
