ec4720ae40e46eef7f7a8d506bf05fa80dfe3843
thadavale <thadavale@ufl.edu>
2023-10-31 18:49:47 -0400

    Merge remote-tracking branch 'origin/main'

--- lab_6_.py
+++ lab_6_.py
@@ -4,7 +4,6 @@
 
 def encode(decoded_numbers):
     encoded_numbers = []
-    global encoded_numbers
     for num in decoded_numbers:
         new_num = int(num) + 3                          # Adds 3 to every number in the decoded numbers list
         if new_num in range (7, 13):
@@ -13,6 +12,17 @@ def encode(decoded_numbers):
     encoded_numbers = ''.join(encoded_numbers)
     return str(encoded_numbers)
 
+# Alice Jiang
+
+def decode(encoded_pass):
+    decoded_pass = ""
+    for num in range(0, len(encoded_pass)):
+        if int(encoded_pass[num]) - 3 < 0:
+            decoded_num = str(10 + (int(encoded_pass[num])) - 3)
+        else:
+            decoded_num = str(int(encoded_pass[num]) - 3)
+        decoded_pass += decoded_num
+    return decoded_pass
 
 # Main Function
 if __name__ == '__main__':
@@ -31,16 +41,5 @@ if __name__ == '__main__':
         if user_input == "3":
             break
 
-# Alice Jiang
-
-def decode(encoded_pass):
-    decoded_pass = ""
-    for num in range(0, len(encoded_pass)):
-        if int(encoded_pass[num]) - 3 < 0:
-            decoded_num = str(10 + (int(encoded_pass[num])) - 3)
-        else:
-            decoded_num = str(int(encoded_pass[num]) - 3)
-        decoded_pass += decoded_num
-    return decoded_pass
 
 
